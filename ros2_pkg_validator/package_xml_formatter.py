4f56873c6add55436d1a760efb1b159f2b2e9ee1
Aljoscha Schmidt <aljoscha.schmidt@tu-darmstadt.de>
2025-04-04 11:45:18 +0200

    Improved order checking

--- ros2_pkg_validator/package_xml_formatter.py
+++ ros2_pkg_validator/package_xml_formatter.py
@@ -4,29 +4,26 @@ import subprocess
 from lxml import etree as ET
 
 
-# Define the expected order of dependency tags
-DEPENDENCY_ORDER = [
-    "buildtool_depend",
-    "buildtool_export_depend",
-    "build_depend",
-    "build_export_depend",
-    "depend",
-    "exec_depend",
-    "doc_depend",
-    "test_depend",
-]
-
-ELEMENT_ORDER = [
-    "name",
-    "version",
-    "description",
-    "maintainer",
-    "license",
-    "url",
-    "author",
-    "group_depend",
-    "member_of_group",
-    "export",
+# Order and min/max occurrences of elements
+ELEMENTS = [
+    ("name", 1, 1),
+    ("version", 1, 1),
+    ("description", 1, 1),
+    ("maintainer", 1, 1),
+    ("license", 1, 1),
+    ("url", 0, 1),
+    ("author", 0, 1),
+    ("buildtool_depend", 0, None),
+    ("buildtool_export_depend", 0, None),
+    ("build_depend", 0, None),
+    ("build_export_depend", 0, None),
+    ("depend", 0, None),
+    ("exec_depend", 0, None),
+    ("doc_depend", 0, None),
+    ("test_depend", 0, None),
+    ("group_depend", 0, 1),
+    ("member_of_group", 0, 1),
+    ("export", 0, 1),
 ]
 
 
@@ -66,18 +63,19 @@ def validate_xml_with_xmllint(xml_file):
 def check_dependency_order(root, xml_file, check_only):
     """Check and optionally correct the order of dependencies in the package.xml file (with comment preservation using lxml)."""
 
-    dependencies = {dep: [] for dep in DEPENDENCY_ORDER}
+    dependency_order = [elm[0] for elm in ELEMENTS if "depend" in elm[0]]
+    dependencies = {dep: [] for dep in dependency_order}
     current_order = []
 
     # Collect dependencies and track their order
     for elem in root:
-        if isinstance(elem.tag, str) and elem.tag in DEPENDENCY_ORDER:
+        if isinstance(elem.tag, str) and elem.tag in dependency_order:
             dependencies[elem.tag].append(elem)
             current_order.append(elem.tag)
 
     # Determine correct order for type grouping
     correct_order = []
-    for dep_type in DEPENDENCY_ORDER:
+    for dep_type in dependency_order:
         correct_order.extend([dep_type] * len(dependencies[dep_type]))
 
     # Check type order mismatch
@@ -101,7 +99,7 @@ def check_dependency_order(root, xml_file, check_only):
         return True
 
     # Remove old dependency elements from root
-    for dep_type in DEPENDENCY_ORDER:
+    for dep_type in dependency_order:
         for elem in dependencies[dep_type]:
             root.remove(elem)
 
@@ -119,7 +117,7 @@ def check_dependency_order(root, xml_file, check_only):
     indendantion = root[0].tail.replace("\n", "")
     # Reinsert sorted dependencies before <export>, <member_of_group>, or <group_depend>
     insert_index = min(export_index, member_of_group_index, group_dep_index)
-    for dep_type in DEPENDENCY_ORDER:
+    for dep_type in dependency_order:
         sorted_elems = sorted(dependencies[dep_type], key=lambda x: x.text)
         for i, elem in enumerate(sorted_elems):
             if i != len(sorted_elems) - 1:
@@ -129,11 +127,6 @@ def check_dependency_order(root, xml_file, check_only):
             root.insert(insert_index, elem)
             insert_index += 1
 
-    # make sure there is no more than one empty line in the xml file
-    for elm in root:
-        while elm.tail and isinstance(elm.tail, str) and elm.tail.startswith("\n\n\n"):
-            elm.tail = elm.tail[1:]
-
     print(f"Corrected dependency order in {xml_file}.")
     return True
 
@@ -141,14 +134,15 @@ def check_dependency_order(root, xml_file, check_only):
 def check_for_duplicates(root, xml_file, check_only):
     """
     Check for duplicate elements in the XML file.
+    -> meaning tag and text are the same
     """
     seen = set()
     duplicates = []
     for elem in root:
-        if elem.tag in seen and elem.tag in ELEMENT_ORDER:
-            duplicates.append(elem.tag)
+        if elem.tag + elem.text.strip() in seen:
+            duplicates.append(elem)
         else:
-            seen.add(elem.tag)
+            seen.add(elem.tag + elem.text.strip())
 
     if duplicates:
         print(f"Duplicate elements found in {xml_file}: {', '.join(duplicates)}")
@@ -159,11 +153,45 @@ def check_for_duplicates(root, xml_file, check_only):
         print(f"No duplicate elements found in {xml_file}.")
         return True
     # Remove duplicates
-    for elem in root:
-        if elem.tag in duplicates:
-            root.remove(elem)
-            duplicates.remove(elem.tag)
+    for elem in duplicates:
+        root.remove(elem)
     return False
+
+
+def check_occurrences(root, xml_file, check_only):
+    """
+    Check the min/max occurrences of elements in the XML file.
+    If check_only is True, only check for errors without correcting.
+    Otherwise, it removes the extra elements.
+    """
+    for elem, min_occurrences, max_occurrences in ELEMENTS:
+        count = len(root.findall(elem))
+        if count < min_occurrences:
+            print(
+                f"Error: Element '{elem}' in {xml_file} has fewer than {min_occurrences} occurrences."
+            )
+            if check_only:
+                return False
+        elif max_occurrences is not None and count > max_occurrences:
+            print(
+                f"Error: Element '{elem}' in {xml_file} has more than {max_occurrences} occurrences."
+            )
+            if check_only:
+                return False
+
+    if check_only:
+        print(f"Occurrences of elements in {xml_file} are correct.")
+        return True
+
+    # Correct the occurrences
+    for elem, min_occurrence, max_occurrences in ELEMENTS:
+        count = len(root.findall(elem))
+        if max_occurrences is not None and count > max_occurrences:
+            for i in range(count - max_occurrences):
+                root.remove(root.find(elem))
+        if count < min_occurrence:
+            print("Please add the missing element: ", elem)
+    return True
 
 
 def check_element_order(root, xml_file, check_only):
@@ -171,17 +199,18 @@ def check_element_order(root, xml_file, check_only):
     Check if the elements in the XML file are in the expected order.
     """
 
-    def find_index(root, tag):
-        for i, elem in enumerate(root):
-            if elem.tag == tag:
+    def find_last_index(root, tag):
+        # return the last index of the tag in the root
+        for i in range(len(root) - 1, -1, -1):
+            if root[i].tag == tag:
                 return i
         return -1
 
-    # c
-    current_order = [elem for elem in root if elem.tag in ELEMENT_ORDER]
+    element_order = [elem[0] for elem in ELEMENTS]
+    current_order = [elem for elem in root if elem.tag in element_order]
     misplaced_elements = []
     for i, elem in enumerate(current_order):
-        if i > 0 and ELEMENT_ORDER.index(elem.tag) < ELEMENT_ORDER.index(
+        if i > 0 and element_order.index(elem.tag) < element_order.index(
             current_order[i - 1].tag
         ):
             misplaced_elements.append(elem)
@@ -203,12 +232,33 @@ def check_element_order(root, xml_file, check_only):
         if elem.tag == "name":  # special case for first element
             prev_index = -1
         else:
-            prev_index = find_index(
-                root, ELEMENT_ORDER[ELEMENT_ORDER.index(elem.tag) - 1]
+            prev_index = find_last_index(
+                root, element_order[element_order.index(elem.tag) - 1]
             )
         root.insert(prev_index + 1, elem)
 
     return True
+
+
+def check_for_empty_lines(root, xml_file, check_only):
+    """
+    Check for empty lines in the XML file.
+    Make sure there are no more than one empty line between elements.
+    """
+
+    for elm in root:
+        if elm.tail and elm.tail.startswith("\n\n\n"):
+            print(f"Error: More than one empty line found in {xml_file}.")
+            if check_only:
+                return False
+
+    if check_only:
+        return True
+
+    # make sure there is no more than one empty line in the xml file
+    for elm in root:
+        while elm.tail and isinstance(elm.tail, str) and elm.tail.startswith("\n\n\n"):
+            elm.tail = elm.tail[1:]
 
 
 def check_and_format(src, check_only):
@@ -225,17 +275,23 @@ def check_and_format(src, check_only):
         tree = ET.parse(xml_file, parser)
         root = tree.getroot()
 
-        # if not check_for_duplicates(root, xml_file, check_only):
-        #    all_valid = False
+        if not check_for_duplicates(root, xml_file, check_only):
+            all_valid = False
+
+        if not check_occurrences(root, xml_file, check_only):
+            all_valid = False
 
         if not check_element_order(root, xml_file, check_only):
             all_valid = False
 
-        # if not check_dependency_order(root, xml_file, check_only):
-        #    all_valid = False
+        if not check_dependency_order(root, xml_file, check_only):
+            all_valid = False
 
-        # Write back to file
-        tree.write(xml_file, encoding="utf-8", xml_declaration=True, pretty_print=True)
+        if not all_valid and not check_only:
+            # Write back to file
+            tree.write(
+                xml_file, encoding="utf-8", xml_declaration=True, pretty_print=True
+            )
 
         # Validate XML structure
         if not validate_xml_with_xmllint(xml_file):
@@ -265,4 +321,7 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    paths = ["/home/aljoscha-schmidt/hector/src/hector_base_velocity_manager"]
+    check_only = True
+    check_and_format(paths, check_only)
